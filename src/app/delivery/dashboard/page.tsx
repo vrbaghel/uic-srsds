afd618a76c3a8aa12da36d2975c0d9b1fe8156f9
Vinayak Baghel <vnyk.baghel@gmail.com>
2025-02-08 18:29:35 -0600

    final changes

--- src/app/delivery/dashboard/page.tsx
+++ src/app/delivery/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { LISTINGS } from '@/utils/consts';
+import { JOBS } from '@/utils/consts';
 import Link from 'next/link';
 
 function DeliveryDashboard() {
@@ -10,9 +10,9 @@ function DeliveryDashboard() {
       <div className="px-10 py-10 w-full">
         <section className="text-gray-600 body-font">
           <div className="flex flex-wrap -m-4">
-            {LISTINGS.map((listing) => (
+            {JOBS.map((job) => (
               <div
-                key={listing.id}
+                key={job.id}
                 className="lg:w-1/3 sm:w-1/2 p-4 hover:cursor-pointer">
                 <div className="flex relative flex-col">
                   <img
@@ -21,9 +21,12 @@ function DeliveryDashboard() {
                     src="https://dummyimage.com/600x360"
                   />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white">
-                    <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">{listing.name}</h2>
-                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{listing.price}</h1>
-                    <p className="leading-relaxed">{listing.address}</p>
+                    <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">{job.name}</h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{job.price}</h1>
+                    <p className="leading-relaxed">{job.address}</p>
+                    <Link href={`/job`} className=''>
+                      <button className="button-primary mt-5">View Job</button>
+                    </Link>
                   </div>
                 </div>
               </div>
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -53,3 +53,42 @@ export const LISTINGS = [
       address: 'Address 6',
     },
   ];
+
+export const JOBS = [
+    {
+      id: 1,
+      name: 'Listing 1',
+      price: '$100',
+      address: 'Address 1',
+    },
+    {
+      id: 2,
+      name: 'Listing 2',
+      price: '$200',
+      address: 'Address 2',
+    },
+    {
+      id: 3,
+      name: 'Listing 3',
+      price: '$15',
+      address: 'Address 3',
+    },
+    {
+      id: 4,
+      name: 'Listing 4',
+      price: '$24',
+      address: 'Address 4',
+    },
+    {
+      id: 5,
+      name: 'Listing 5',
+      price: '$65',
+      address: 'Address 5',
+    },
+    {
+      id: 6,
+      name: 'Listing 6',
+      price: '$40',
+      address: 'Address 6',
+    },
+  ];
